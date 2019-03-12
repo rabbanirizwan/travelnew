3b3377525804314b9184562aae32a5da7632535a
rabbanirizwan <rabbani_rizwan@yahoo.com>
2019-03-12 23:06:25 +0500

    fourth

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+
+
+
+
 $('.owl-carousel').owlCarousel({
     
     loop:true,
@@ -7,10 +11,10 @@ $('.owl-carousel').owlCarousel({
         0:{
             items:1
         },
-        400:{
+        600:{
             items:2
         },
-        900:{
+        800:{
             items:2
         },
         1100:{
@@ -25,6 +29,9 @@ $('.owl-carousel').owlCarousel({
         }
     }
 })
+
+
+
 var cardNumber= ["one","two","three","four","five"];
 var guiderNumber = ["guider-one","guider-two","guider-three","guider-four","guider-five"];
 function  cardSlider()
